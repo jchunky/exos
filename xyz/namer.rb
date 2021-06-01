fd9eeb41a8e0bfec297622b98094f137a9d0a0f6
Chunky <jasonc@nulogy.com>
2021-06-01 04:46:07 -0400

    refactor

--- xyz/namer.rb
+++ xyz/namer.rb
@@ -1,25 +1,47 @@
-require 'digest'
+require "securerandom"
 
 module XYZ
-  module Namer
-
-    def self.xyz_filename(target)
-      # File format:
-      # [day of month zero-padded][three-letter prefix] \
-      # _[kind]_[age_if_kind_personal]_[target.id] \
-      # _[8 random chars]_[10 first chars of title].jpg
-      filename = "#{target.publish_on.strftime("%d")}"
-      filename << "#{target.xyz_category_prefix}"
-      filename << "#{target.kind.gsub("_", "")}"
-      filename << "_%03d" % (target.age || 0) if target.personal?
-      filename << "_#{target.id.to_s}"
-      filename << "_#{Digest::SHA1.hexdigest(rand(10000).to_s)[0,8]}"
-      truncated_title = target.title.gsub(/[^\[a-z\]]/i, '').downcase
-      truncate_to = truncated_title.length > 9 ? 9 : truncated_title.length
-      filename << "_#{truncated_title[0..(truncate_to)]}"
-      filename << ".jpg"
-      return filename
+  class Namer < Struct.new(:file)
+    def self.xyz_filename(file)
+      new(file).filename
     end
 
+    def filename
+      "#{basename}.jpg"
+    end
+
+    private
+
+    def basename
+      [prefix, age, file.id, noise, title].compact.join("_")
+    end
+
+    def prefix
+      [publication_day, category, kind].join
+    end
+
+    def publication_day
+      file.publish_on.strftime("%d")
+    end
+
+    def category
+      file.xyz_category_prefix
+    end
+
+    def kind
+      file.kind.gsub("_", "")
+    end
+
+    def age
+      format("%03d", file.age || 0) if file.personal?
+    end
+
+    def noise
+      SecureRandom.hex(4)
+    end
+
+    def title
+      file.title.downcase.gsub(/[^\[a-z\]]/, "")[0, 10]
+    end
   end
 end
