3bb0207f2a73e7bfa68279a113e8745bbe9f5a02
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-20 21:19:32 -0400

    refactor

--- xyz/namer.rb
+++ xyz/namer.rb
@@ -1,9 +1,15 @@
 require "securerandom"
 
 module XYZ
-  class Namer < Struct.new(:file)
+  class Namer
+    attr_reader :file
+
     def self.xyz_filename(file)
       new(file).filename
+    end
+
+    def initialize(file)
+      @file = file
     end
 
     def filename
@@ -12,13 +18,13 @@ module XYZ
 
     private
 
-    def prefix          = [publication_day, category, kind].join
+    def prefix = [publication_day, category, kind].join
     def publication_day = file.publish_on.strftime("%d")
-    def category        = file.xyz_category_prefix
-    def kind            = file.kind.delete("_")
-    def age             = file.personal? ? format("%03d", file.age.to_i) : nil
-    def noise           = SecureRandom.hex(4)
-    def title           = sanitized_title[0, 10]
+    def category = file.xyz_category_prefix
+    def kind = file.kind.delete("_")
+    def age = file.personal? ? format("%03d", file.age.to_i) : nil
+    def noise = @noise ||= SecureRandom.hex(4)
+    def title = sanitized_title[0, 10]
     def sanitized_title = file.title.downcase.gsub(/[^\[a-z\]]/, "")
   end
 end
