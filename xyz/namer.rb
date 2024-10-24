11504ecd0d67ed8d22f977412b43ba3224e36300
Jason CheongKeeYou <jasonc@nulogy.com>
2024-10-24 01:31:55 -0400

    refactor

--- xyz/namer.rb
+++ xyz/namer.rb
@@ -8,13 +8,13 @@ module XYZ
 
     private
 
-    def prefix          = [publication_day, category, kind].join
+    def prefix = [publication_day, category, kind].join
     def publication_day = file.publish_on.strftime("%d")
-    def category        = file.xyz_category_prefix
-    def kind            = file.kind.delete("_")
-    def age             = file.personal? ? format("%03d", file.age.to_i) : nil
-    def noise           = SecureRandom.hex(4)
-    def title           = sanitized_title[0, 10]
+    def category = file.xyz_category_prefix
+    def kind = file.kind.delete("_")
+    def age = file.personal? ? format("%03d", file.age.to_i) : nil
+    def noise = SecureRandom.hex(4)
+    def title = sanitized_title[0, 10]
     def sanitized_title = file.title.downcase.gsub(/[^\[a-z\]]/, "")
   end
 end
